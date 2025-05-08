d4262b7713d0b72cc6597927ec881e32f10931ae
kobaken <kentafly88@gmail.com>
2025-05-08 18:33:03 +0900

    refactor: createApp

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import { Hono } from 'hono'
-import { logger } from 'hono/logger'
-import { requestId } from 'hono/request-id'
-import { asyncContext, getContext, Env } from '@getcronit/pylon'
+import { createApp } from './util'
+import { getContext } from '@getcronit/pylon'
 
 import { User } from './repo/user'
 
-const app = new Hono<Env>()
-app.use(logger())
-app.use(requestId())
-
-// Save the request context in the async context
-app.use('*', async (c, next) => {
-  return new Promise((resolve, reject) => {
-    asyncContext.run(c, async () => {
-      try {
-        resolve(await next())
-      } catch (error) {
-        reject(error)
-      }
-    })
-  })
-})
+const app = createApp();
 
 export const graphql = {
   Query: {
@@ -33,15 +16,5 @@ export const graphql = {
   },
   Mutation: {}
 }
-
-// Turn off playground and viewer for production
-// If Pylon is upgraded to v3, then it can disabled by configuration.
-// Ref: https://github.com/getcronit/pylon/issues/72
-app.get('/graphql', async (c, next) => {
-  if (c.env.APP_ENV === 'production') {
-    return c.notFound()
-  }
-  await next()
-})
 
 export default app
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,44 @@
-import { getContext } from '@getcronit/pylon'
+import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { requestId } from 'hono/request-id'
+
+import { asyncContext, getContext, Env } from '@getcronit/pylon'
 import { drizzle } from 'drizzle-orm/node-postgres'
 
 export const getDB = function () {
   const c = getContext()
   const db = drizzle(c.env.DATABASE_URL)
   return db;
+}
+
+export const createApp = function () {
+
+  const app = new Hono<Env>()
+  app.use(logger())
+  app.use(requestId())
+
+  // Save the request context in the async context
+  app.use('*', async (c, next) => {
+    return new Promise((resolve, reject) => {
+      asyncContext.run(c, async () => {
+        try {
+          resolve(await next())
+        } catch (error) {
+          reject(error)
+        }
+      })
+    })
+  })
+
+  // Turn off playground and viewer for production
+  // If Pylon is upgraded to v3, then it can disabled by configuration.
+  // Ref: https://github.com/getcronit/pylon/issues/72
+  app.get('/graphql', async (c, next) => {
+    if (c.env.APP_ENV === 'production') {
+      return c.notFound()
+    }
+    await next()
+  })
+
+  return app;
 }
